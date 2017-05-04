08aa138a9cdee45a66a4c4ff62708efebcffaeb8
bluevolley6 <kathrynk263@yahoo.com>
2017-05-04 10:44:13 -0500

    Packaged android

--- Build/Android/src/com/FallingUp/FallingUpVR/OBBData.java
+++ Build/Android/src/com/FallingUp/FallingUpVR/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-1671504215L // the length of the file in bytes
+1671629774L // the length of the file in bytes
 )
 };
 };
